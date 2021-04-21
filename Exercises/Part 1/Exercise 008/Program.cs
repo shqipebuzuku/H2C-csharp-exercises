898db805deac215e216464a2f82bd7add547d899
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-22 01:47:47 +0300

    Update Program.cs

--- Exercises/Part 1/Exercise 008/Program.cs
+++ Exercises/Part 1/Exercise 008/Program.cs
@@ -6,7 +6,9 @@ namespace exercise_08
     {
         public static void Main(string[] args)
         {
-            // Write your code here:
+            Console.WriteLine("What is your name?");
+            string ask = Console.ReadLine();
+            Console.WriteLine("Hello " + ask + "!");
 
         }
     }
