c536f000ca00981bef75d5789e0b93ebe3ece739
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-22 18:36:01 +0300

    Update Program.cs

--- Exercises/Part 1/Exercise 015/Program.cs
+++ Exercises/Part 1/Exercise 015/Program.cs
@@ -7,7 +7,29 @@ namespace exercise_15
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+            Console.WriteLine("Give a string:");
+            string sent = Console.ReadLine();
+            
+            int num1;
+            Console.WriteLine("Give an integer:");
+            string i = Console.ReadLine();
+            num1 = Convert.ToInt32(i);
+
+            Double num2;
+            Console.WriteLine("Give me a double:");
+            string j = Console.ReadLine();
+            num2 = Convert.ToDouble(j);
+
+            Boolean num3;
+            Console.WriteLine("Give me a boolean:");
+            string k = Console.ReadLine();
+            num3 = Convert.ToBoolean(k);
+
+            Console.WriteLine("Your string: " + sent);
+            Console.WriteLine("Your integer: "+num1);
+            Console.WriteLine("Your double: " + num2);
+            Console.WriteLine("Your boolean: " + num3);
+
 
     }
   }
