eb6719c5d4a1072256bb3f49e643052370185d59
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-22 23:26:55 +0300

    Update Program.cs

--- Exercises/Part 1/Exercise 031/Program.cs
+++ Exercises/Part 1/Exercise 031/Program.cs
@@ -6,7 +6,18 @@ namespace exercise_31
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+            int num1;
+            int mod;
+
+
+            Console.WriteLine("Give an integer");
+            string i = Console.ReadLine();
+            num1 = Convert.ToInt32(i);
+
+            mod = num1 % 2;
+
+            if (mod == 0 ) { Console.WriteLine("It is even."); };
+            if (mod == 1) { Console.WriteLine("It is odd."); };
 
     }
   }
