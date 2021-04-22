96466a1349920e18de7e3f90ed5db07e1fc87535
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-22 19:44:55 +0300

    Update Program.cs

--- Exercises/Part 1/Exercise 025/Program.cs
+++ Exercises/Part 1/Exercise 025/Program.cs
@@ -6,7 +6,13 @@ namespace exercise_25
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+            int num1;
+
+            Console.WriteLine("Give a number ");
+            string i = Console.ReadLine();
+            num1 = Convert.ToInt32(i);
+
+            if (num1 == 1984) { Console.WriteLine("Orwell"); };
 
     }
   }
