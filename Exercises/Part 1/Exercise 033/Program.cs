852eabf0515e66471b0c3dcb1ef5f525f2b3eb56
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-22 23:54:00 +0300

    Update Program.cs

--- Exercises/Part 1/Exercise 033/Program.cs
+++ Exercises/Part 1/Exercise 033/Program.cs
@@ -6,7 +6,14 @@ namespace exercise_33
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+            Console.WriteLine("Give the first string:");
+            string i = Console.ReadLine();
+            Console.WriteLine("Give the second string:");
+            string j = Console.ReadLine();
+
+            if (i == j ) { Console.WriteLine("Echo!"); };
+            if (i != j) { Console.WriteLine("Nope!"); };
+
 
     }
   }
