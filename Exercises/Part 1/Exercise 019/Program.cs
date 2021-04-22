1e0504e5af4457a41a867ad59d39f8c5df26f07a
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-22 19:03:49 +0300

    Update Program.cs

--- Exercises/Part 1/Exercise 019/Program.cs
+++ Exercises/Part 1/Exercise 019/Program.cs
@@ -7,7 +7,20 @@ namespace exercise_19
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+            int num1;
+            int num2;
+ 
+            int sum;
+            Console.WriteLine("Give me the first number");
+            string i = Console.ReadLine();
+            num1 = Convert.ToInt32(i);
+            Console.WriteLine("Give me the second number");
+            string j = Console.ReadLine();
+            num2 = Convert.ToInt32(j);
+
+            sum = num1 + num2 ;
+
+            Console.WriteLine(i + " + " + j +" = "+sum);
 
     }
   }
