32ef905c026c80a250d2a276e275ac1512a9d097
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-23 00:26:43 +0300

    Update Program.cs

--- Exercises/Part 1/Exercise 039/Program.cs
+++ Exercises/Part 1/Exercise 039/Program.cs
@@ -6,7 +6,18 @@ namespace exercise_39
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+            int numb = 10;
+            int count = 0;
+            while (numb != 0)
+            {
+                Console.WriteLine("Give a number:");
+                string i = Console.ReadLine();
+                numb = Convert.ToInt32(i);
+                if (numb != 0) { count=count+numb; };
+                
+            };
+
+            Console.WriteLine("Total sum of numbers: " + count);
     }
   }
 }
