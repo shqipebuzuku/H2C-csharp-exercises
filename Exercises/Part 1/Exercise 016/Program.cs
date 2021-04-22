010a5aae80afd7da65134f9f9e78885e19660e39
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-22 18:44:51 +0300

    Update Program.cs

--- Exercises/Part 1/Exercise 016/Program.cs
+++ Exercises/Part 1/Exercise 016/Program.cs
@@ -7,7 +7,14 @@ namespace exercise_16
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+            int day;
+            int secs;
+            Console.WriteLine("How many days?");
+            string i = Console.ReadLine();
+            day = Convert.ToInt32(i);
+            secs = day * 24 * 60 * 60;
+
+            Console.WriteLine(day + " days equals " + secs + " seconds");
 
     }
   }
