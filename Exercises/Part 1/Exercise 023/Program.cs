4a103b8437de3881eaf49700affd46ac5800e370
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-22 19:34:58 +0300

    Update Program.cs

--- Exercises/Part 1/Exercise 023/Program.cs
+++ Exercises/Part 1/Exercise 023/Program.cs
@@ -7,7 +7,28 @@ namespace exercise_23
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+            Double num1;
+            Double num2;
+            Double sum;
+            Double min;
+            Double mul;
+            Double div;
+            Console.WriteLine("Give me the first number");
+            string i = Console.ReadLine();
+            num1 = Convert.ToDouble(i);
+            Console.WriteLine("Give me the second number");
+            string j = Console.ReadLine();
+            num2 = Convert.ToDouble(j);
+
+            sum = num1 + num2;
+            min = num1 - num2;
+            mul = num1 * num2;
+            div = num1 / num2;
+
+            Console.WriteLine(i + " + " + j + " = " + sum);
+            Console.WriteLine(i + " - " + j + " = " + min);
+            Console.WriteLine(i + " * " + j + " = " + mul);
+            Console.WriteLine(i + " / " + j + " = " + div);
 
     }
   }
