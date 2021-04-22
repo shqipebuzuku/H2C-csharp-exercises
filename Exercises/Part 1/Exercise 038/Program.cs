42fc42f08a79b09d4c79d2599acf82b3b6d533aa
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-23 00:20:33 +0300

    Update Program.cs

--- Exercises/Part 1/Exercise 038/Program.cs
+++ Exercises/Part 1/Exercise 038/Program.cs
@@ -6,7 +6,18 @@ namespace exercise_38
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+            int numb = 100;
+            int count = 0;
+            while (numb != 0)
+            {
+                Console.WriteLine("Give a number:");
+                string i = Console.ReadLine();
+                numb = Convert.ToInt32(i);
+                if (numb < 0) { count++; };
+                
+            };
+
+            Console.WriteLine("Total amount of negative numbers: " + count);
 
     }
   }
