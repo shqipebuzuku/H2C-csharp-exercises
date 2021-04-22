9b65512d7b6ab470972d56a51c910d90e0ad7a3e
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-22 19:22:19 +0300

    Update Program.cs

--- Exercises/Part 1/Exercise 022/Program.cs
+++ Exercises/Part 1/Exercise 022/Program.cs
@@ -7,7 +7,23 @@ namespace exercise_22
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+            Double num1;
+            Double num2;
+            Double num3;
+            Double avg;
+            Console.WriteLine("Give me the first number");
+            string i = Console.ReadLine();
+            num1 = Convert.ToDouble(Convert.ToInt32(i));
+            Console.WriteLine("Give me the second number");
+            string j = Console.ReadLine();
+            num2 = Convert.ToDouble(Convert.ToInt32(j));
+            Console.WriteLine("Give me the third number");
+            string k = Console.ReadLine();
+            num3 = Convert.ToDouble(Convert.ToInt32(k));
+
+            avg = (num1 + num2 + num3)/ 3;
+
+            Console.WriteLine("The average is: "+ avg);
 
     }
   }
