4f2bc06786389f380f64b5cd93151997ec170a5d
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-22 19:11:16 +0300

    Update Program.cs

--- Exercises/Part 1/Exercise 021/Program.cs
+++ Exercises/Part 1/Exercise 021/Program.cs
@@ -7,7 +7,20 @@ namespace exercise_21
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+            Double num1;
+            Double num2;
+ 
+            Double avg;
+            Console.WriteLine("Give me the first number");
+            string i = Console.ReadLine();
+            num1 = Convert.ToDouble(i);
+            Console.WriteLine("Give me the second number");
+            string j = Console.ReadLine();
+            num2 = Convert.ToDouble(j);
+
+            avg = (num1 + num2)/ 2;
+
+            Console.WriteLine("The average is: "+ avg);
 
     }
   }
