99465f1704daf255807bedcbc6b6e6a05813228e
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-22 19:00:34 +0300

    Update Program.cs

--- Exercises/Part 1/Exercise 018/Program.cs
+++ Exercises/Part 1/Exercise 018/Program.cs
@@ -7,7 +7,22 @@ namespace exercise_18
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+            int num1;
+            int num2;
+            int num3;
+            int sum;
+            Console.WriteLine("Give me the first number");
+            string i = Console.ReadLine();
+            num1 = Convert.ToInt32(i);
+            Console.WriteLine("Give me the second number");
+            string j = Console.ReadLine();
+            num2 = Convert.ToInt32(j);
+            Console.WriteLine("Give me the third number");
+            string k = Console.ReadLine();
+            num3 = Convert.ToInt32(k);
+            sum = num1 + num2 + num3;
+
+            Console.WriteLine("The sum is "+sum);
 
     }
   }
