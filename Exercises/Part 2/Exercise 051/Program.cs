b92a17fa05694f6e312be3504345879af9260f37
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-25 02:01:17 +0300

    Update Program.cs

--- Exercises/Part 2/Exercise 051/Program.cs
+++ Exercises/Part 2/Exercise 051/Program.cs
@@ -6,12 +6,21 @@ namespace exercise_51
   {
     public static void Main(String[] args)
     {
-      // Call your method here:
+            int numb1;
+            
+            Console.WriteLine("What number?");
+            string i = Console.ReadLine();
+            numb1 = Convert.ToInt32(i);
 
-
+            PrintUntilNumber(numb1); 
     }
 
-    // Write your method here:
-
+   public static void PrintUntilNumber(int a)
+    {
+            int count;
+            for (count = 1; count <= a; count++) {  
+            Console.WriteLine(count);
+            };
+    }
   }
 }
