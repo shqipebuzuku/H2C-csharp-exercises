9b9151783519b864f186f6af39bb414d12d61b07
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-25 00:19:40 +0300

    Update Program.cs

--- Exercises/Part 2/Exercise 047/Program.cs
+++ Exercises/Part 2/Exercise 047/Program.cs
@@ -6,9 +6,31 @@ namespace exercise_47
   {
     public static void Main(string[] args)
     {
+            // Section 1
+            Console.WriteLine("Section 1");
+            int numb1;
+            int count = 1;
 
-      // Write your code here:
-      
+            Console.WriteLine("Where to?");
+            string i = Console.ReadLine();
+            numb1 = Convert.ToInt32(i);
+
+            while (count <= numb1) { Console.WriteLine(count); count++; };
+
+            // Section 2
+            Console.WriteLine("Section 2");
+            int numb2;
+            int numb3;
+
+            Console.WriteLine("Where to?");
+            string j = Console.ReadLine();
+            numb2 = Convert.ToInt32(j);
+
+            Console.WriteLine("Where from?");
+            string k = Console.ReadLine();
+            numb3 = Convert.ToInt32(k);
+
+            while (numb3 <= numb2) { Console.WriteLine(numb3); numb3++; };
     }
   }
 }
