5b67912e0e74e9ddb2a4268f283891d07029579b
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-25 00:48:04 +0300

    Update Program.cs

--- Exercises/Part 2/Exercise 048/Program.cs
+++ Exercises/Part 2/Exercise 048/Program.cs
@@ -6,8 +6,37 @@ namespace exercise_48
   {
     public static void Main(string[] args)
     {
+            int numb1 = 0;
+            int refe = -1;
+            int sum = 0;
+            int count = 0;
+            double avg = 0;
+            int even = 0;
+            int odd = 0;
+            int check;
 
-      // Write your code here:
+            while (numb1 != refe)
+            {
+                
+                Console.WriteLine("Give Number:");
+                string i = Console.ReadLine();
+                numb1 = Convert.ToInt32(i);
+                if (numb1 != -1)
+                {
+                    count++;
+                    sum = sum + numb1;
+                    avg = sum / count;
+                    check = numb1 % 2;
+                    if (check == 0) { even++; };
+                    if (check != 0) { odd++; };
+                }
+            };
+            Console.WriteLine("Thx! Bye!");
+            Console.WriteLine("Sum: " + sum);
+            Console.WriteLine("Numbers: " + count);
+            Console.WriteLine("Average: " + avg);
+            Console.WriteLine("Even: " + even);
+            Console.WriteLine("Odd: " + odd);
  
     }
   }
