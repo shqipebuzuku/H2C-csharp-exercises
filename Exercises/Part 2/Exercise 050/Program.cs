fbe32030b0db8293f8838a140284d4ba904bb83b
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-25 01:22:30 +0300

    Update Program.cs

--- Exercises/Part 2/Exercise 050/Program.cs
+++ Exercises/Part 2/Exercise 050/Program.cs
@@ -6,13 +6,18 @@ namespace exercise_50
   {
     public static void Main(string[] args)
     {
-      // Call your method here:
+            int numb1;
+            int count;
+            Console.WriteLine("How many times?");
+            string i = Console.ReadLine();
+            numb1 = Convert.ToInt32(i);
+            for(count = 0;count < numb1;count++) { PrintPhrase(); };
  
     }
 
     // Write your method here:
     public static void PrintPhrase() {
-
+            Console.WriteLine("In a hole in the ground there lived a method");
     }
   }
 }
