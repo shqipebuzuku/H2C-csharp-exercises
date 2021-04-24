92adb1df23a431f5b7cb6d7039071f51f3eb417b
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-25 00:00:30 +0300

    Update Program.cs

--- Exercises/Part 2/Exercise 044/Program.cs
+++ Exercises/Part 2/Exercise 044/Program.cs
@@ -7,7 +7,19 @@ namespace exercise_44
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+            int numb1;
+            int numb2;
+
+            Console.WriteLine("Give first number:");
+            string i = Console.ReadLine();
+            numb1 = Convert.ToInt32(i);
+            Console.WriteLine("Give second number:");
+            string j = Console.ReadLine();
+            numb2 = Convert.ToInt32(j);
+            if (numb1 < numb2) { Console.WriteLine(numb1+ " is less than " + numb2); };
+            if (numb1 > numb2) { Console.WriteLine(numb1 + " is grEater than " + numb2); };
+            if (numb1 == numb2) { Console.WriteLine(numb1 + " is equal to " + numb2); };
+            
 
     }
   }
