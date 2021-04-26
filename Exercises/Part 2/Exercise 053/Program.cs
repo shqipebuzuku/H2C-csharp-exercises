68b89621afd30911ca358c04503fafd04ab3fa8e
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-26 19:02:08 +0300

    Update Program.cs

--- Exercises/Part 2/Exercise 053/Program.cs
+++ Exercises/Part 2/Exercise 053/Program.cs
@@ -2,16 +2,34 @@
 
 namespace exercise_53
 {
-  class Program
-  {
-    public static void Main(String[] args)
+   class Program
     {
-      // Call your method here:
+        static void Main(string[] args)
+        {
+            int numb1;
+            int numb2;
+            
+            Console.WriteLine("Give numerator");
+            string i = Console.ReadLine();
+            numb1 = Convert.ToInt32(i);
 
+            Console.WriteLine("Give denominator");
+            string j = Console.ReadLine();
+            numb2 = Convert.ToInt32(j);
 
+            Division(numb1,numb2); 
+            
+        }
+        public static void Division(int numerator, int denominator)
+        {
+            double div;
+            double num = Convert.ToDouble(numerator);
+            double den = Convert.ToDouble(denominator);
+
+            div = num / den;
+            Console.WriteLine("The result of the division is:");
+            Console.WriteLine(div);
+            
+        }
     }
-
-    // Write your method here:
-
-  }
 }
