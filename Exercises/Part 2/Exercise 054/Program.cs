30e7ce0f3bcb9b7c8e075e1a3f50020f063d2e9c
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-26 19:10:48 +0300

    Update Program.cs

--- Exercises/Part 2/Exercise 054/Program.cs
+++ Exercises/Part 2/Exercise 054/Program.cs
@@ -2,15 +2,32 @@
 
 namespace exercise_54
 {
-  class Program
-  {
-    public static void Main(String[] args)
+    class Program
     {
-      // Call your method here:
+        static void Main(string[] args)
+        {
+            int numb1;
+            int numb2;
+            
+            Console.WriteLine("Give start");
+            string i = Console.ReadLine();
+            numb1 = Convert.ToInt32(i);
 
+            Console.WriteLine("Give end");
+            string j = Console.ReadLine();
+            numb2 = Convert.ToInt32(j);
+
+            DivisibleByThreeInRange(numb1,numb2); 
+            
+        }
+        public static void DivisibleByThreeInRange(int beginning, int end)
+        {
+
+            for (; beginning <= end; beginning++)
+            {
+                if((beginning%3) == 0) { Console.WriteLine(beginning); }
+            }
+                    
+        }
     }
-
-    // Write your method here:
-
-  }
 }
