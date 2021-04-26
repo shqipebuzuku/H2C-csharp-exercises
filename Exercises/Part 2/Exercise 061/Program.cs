40a070f835dd143de6ccae876425eae5c66539e1
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-27 01:26:42 +0300

    Update Program.cs

--- Exercises/Part 2/Exercise 061/Program.cs
+++ Exercises/Part 2/Exercise 061/Program.cs
@@ -2,31 +2,67 @@
 
 namespace exercise_61
 {
-  class Program
-  {
-    public static void Main(String[] args)
+    class Program
     {
 
-    }
+        public static void Main(String[] args)
+        {
+            // Section 1
+            PrintSpaces(5);
+            Console.WriteLine("");
 
-    public static void PrintStars(int number)
-    {
+            // Section 2
+            PrintRightTriangle(4);
+            Console.WriteLine("");
 
-    }
+            // Section 3
+            ChristmasTree(10);
+            Console.WriteLine("");
 
-    public static void PrintSpaces(int number)
-    {
+        }
 
-    }
+        public static void PrintStars(int number1)
+        {
+            string tabs = new String('*', number1);
+            Console.WriteLine(tabs);
+            Console.WriteLine("");
+        }
 
-    public static void PrintRightTriangle(int size)
-    {
+        public static void PrintSpaces(int number1)
+        {
+            string tabs = new String(' ', number1);
+            Console.WriteLine(tabs);
+            
+        }
 
-    }
 
-    public static void ChristmasTree(int height)
-    {
+        public static void PrintRightTriangle(int size)
+        {
+            int count2 = size;
 
-    }
-  }
+            for (int count = 1; count <= size; count++)
+            {
+                count2--;
+                string tabs = String.Concat(new String(' ', count2), new String('*', count));
+                Console.WriteLine(tabs);
+
+            }
+
+        }
+        public static void ChristmasTree(int size)
+        {
+            int count2 = size;
+            int count3 = 0;
+
+            for (int count = 1; count <= size; count++)
+            {
+                count2--;
+                string tabs = String.Concat(new String(' ', count2), new String('*', count+count3));
+                Console.WriteLine(tabs);
+                count3++;
+            }
+            string tabs2 = String.Concat(new String(' ', size-2), new String('*', 3));
+            Console.WriteLine(tabs2);
+            Console.WriteLine(tabs2);
+        }
 }
