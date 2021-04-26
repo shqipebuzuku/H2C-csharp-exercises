7f37af5fe407034cc2ed3674e2a63a49a4ddfee0
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-26 19:52:44 +0300

    Update Program.cs

--- Exercises/Part 2/Exercise 058/Program.cs
+++ Exercises/Part 2/Exercise 058/Program.cs
@@ -2,15 +2,34 @@
 
 namespace exercise_58
 {
-  class Program
-  {
-    public static void Main(String[] args)
+    class Program
     {
-      int answer = Smallest(2, 7);
-      Console.WriteLine("Smallest: " + answer);
-    }
 
-    // Write your method here:
-    
-  }
+        public static void Main(String[] args)
+        {
+            int numb1;
+            int numb2;
+
+
+            Console.WriteLine("Give number");
+            string i = Console.ReadLine();
+            numb1 = Convert.ToInt32(i);
+
+            Console.WriteLine("Give number");
+            string j = Console.ReadLine();
+            numb2 = Convert.ToInt32(j);
+
+            int answer = Smallest(numb1, numb2);
+            Console.WriteLine("Smallest: " + answer);
+        }
+
+        public static int Smallest(int number1, int number2)
+        {
+            int result = 0;
+            if(number1 < number2) { result = number1; }
+            if(number2 < number1) { result = number2; }
+
+            return result;
+
+        }
 }
