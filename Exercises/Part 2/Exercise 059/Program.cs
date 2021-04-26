6dd8b8f4a909d6c270331ab625bd88fe9eb89dcf
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-26 20:02:02 +0300

    Update Program.cs

--- Exercises/Part 2/Exercise 059/Program.cs
+++ Exercises/Part 2/Exercise 059/Program.cs
@@ -2,15 +2,52 @@
 
 namespace exercise_59
 {
-  class Program
-  {
-    public static void Main(String[] args)
+    class Program
     {
-      int answer = Greatest(2, 7, 3);
-      Console.WriteLine("Greatest: " + answer);
+
+        public static void Main(String[] args)
+        {
+            int numb1;
+            int numb2;
+            int numb3;
+
+            Console.WriteLine("Give number");
+            string i = Console.ReadLine();
+            numb1 = Convert.ToInt32(i);
+
+            Console.WriteLine("Give number");
+            string j = Console.ReadLine();
+            numb2 = Convert.ToInt32(j);
+
+            Console.WriteLine("Give number");
+            string k = Console.ReadLine();
+            numb3 = Convert.ToInt32(k);
+
+            int answer = Greatest(numb1, numb2, numb3);
+            Console.WriteLine("GrEatest: " + answer);
+        }
+
+        public static int Greatest(int number1, int number2, int number3)
+        {
+            int result = 0;
+            if(number1 > number2) 
+            {
+                if (number1 > number3) { result = number1; }
+            }
+
+            if (number2 > number1)
+            {
+                if (number2 > number3) { result = number2; }
+            }
+
+            if (number3 > number1)
+            {
+                if (number3 > number2) { result = number3; }
+            }
+
+            return result;
+
+        }
+
     }
-
-    // Write your method here:
-
-  }
 }
