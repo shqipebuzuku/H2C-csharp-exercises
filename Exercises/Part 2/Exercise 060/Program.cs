10245c33641179c226d63232eb97d99d50fcf9ea
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-26 21:20:06 +0300

    Update Program.cs

--- Exercises/Part 2/Exercise 060/Program.cs
+++ Exercises/Part 2/Exercise 060/Program.cs
@@ -2,36 +2,60 @@
 
 namespace exercise_60
 {
-  class Program
-  {
+    class Program
+    {
+
         public static void Main(String[] args)
-    {
+        {
+            // Section 1
+            PrintStars(5);
+            PrintStars(3);
+            PrintStars(9);
 
+            // Section 2
+            PrintSquare(4);
+
+            // Section 3
+            PrintRectangle(17, 3);
+
+            // Section 3
+            PrintTriangle(4);
+        }
+
+        public static void PrintStars(int number1)
+        {
+            string tabs = new String('*', number1);
+            Console.WriteLine(tabs);
+            Console.WriteLine("");
+        }
+
+        public static void PrintSquare(int number1)
+        {
+            
+            string tabs = new String('*', number1);
+            for(int count = 1;count <= number1; count++) { Console.WriteLine(tabs); }
+
+            Console.WriteLine("");
+
+        }
+        public static void PrintRectangle(int width, int height)
+        {
+
+            string tabs = new String('*', width);
+            for (int count = 1; count <= height; count++) { Console.WriteLine(tabs); }
+
+            Console.WriteLine("");
+
+        }
+
+        public static void PrintTriangle(int size)
+        {
+
+            
+            for (int count = 1; count <= size; count++) { PrintStars(count); }
+
+            Console.WriteLine("");
+
+        }
     }
-
-
-    public static void PrintStars(int number)
-    {
-      // you can print one star with the command
-      // Console.Write("*");
-      // call the print command n times
-      // in the end print a line break with the comand
-      // Console.WriteLine("");
-    }
-
-    public static void PrintSquare(int size)
-    {
-
-    }
-
-    public static void PrintRectangle(int width, int height)
-    {
-
-    }
-
-    public static void PrintTriangle(int size)
-    {
-
-    }
-  }
 }
