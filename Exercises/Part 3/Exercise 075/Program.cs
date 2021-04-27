79055f7f04c23116a9fd7c44f7a8172557a5f737
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-28 00:58:35 +0300

    Update Program.cs

--- Exercises/Part 3/Exercise 075/Program.cs
+++ Exercises/Part 3/Exercise 075/Program.cs
@@ -3,24 +3,44 @@ using System.Collections.Generic;
 
 namespace exercise_75
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
+        public static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            while (true)
+            {
+                int input = Convert.ToInt32(Console.ReadLine());
+                if (input == -1)
+                {
+                    break;
+                }
+                list.Add(input);
+            }
 
 
-      // Example method calls for testing your method.
-      // List<int> numbers = new List<int>();
-      // numbers.Add(5);
+            // Example method calls for testing your method.
+            // List<int> numbers = new List<int>();
+            // numbers.Add(5);
 
-      Console.WriteLine("The numbers in the range [0, 5]");
-      PrintNumbersInRange(numbers, 0, 5);
+            Console.WriteLine("The numbers in the range [0, 5]");
+            PrintNumbersInRange(list, 0, 5);
 
-      Console.WriteLine("The numbers in the range [3, 10]");
-      PrintNumbersInRange(numbers, 3, 10);
+            Console.WriteLine("The numbers in the range [3, 10]");
+            PrintNumbersInRange(list, 3, 10);
+        }
+
+        public static void PrintNumbersInRange(List<int> number, int low, int high)
+        {
+            int num = number.Count;
+            
+            for (int count = 0; count < num; count++)
+            {
+                if (number[count] >= low && number[count] <= high) { Console.WriteLine(number[count]); }
+            }
+
+        }
+
     }
-
-    }
-  }
 }
 
