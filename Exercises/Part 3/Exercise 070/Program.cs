b761641d72ccbbb5a626fc1e12b8caf2745ae9c6
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-28 00:06:17 +0300

    Update Program.cs

--- Exercises/Part 3/Exercise 070/Program.cs
+++ Exercises/Part 3/Exercise 070/Program.cs
@@ -3,20 +3,29 @@ using System.Collections.Generic;
 
 namespace exercise_70
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      List<int> list = new List<int>();
-      while (true)
-      {
-        int input = Convert.ToInt32(Console.ReadLine());
-        if (input == -1)
+        public static void Main(string[] args)
         {
-          break;
+            List<int> list = new List<int>();
+            while (true)
+            {
+                int input = Convert.ToInt32(Console.ReadLine());
+                if (input == -1)
+                {
+                    break;
+                }
+                list.Add(input);
+            }
+            int num = list.Count;
+
+            int high = 0;
+            for (int count = 0;count < num; count++) 
+            { 
+                if (list[count]>=high ) { high = list[count];  }
+                
+            }
+            Console.WriteLine(high);
         }
-        list.Add(input);
-      }
     }
-  }
 }
