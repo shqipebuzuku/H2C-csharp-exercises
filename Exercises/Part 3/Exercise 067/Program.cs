454cc61f504b33f8db775a93b3c414e67185b555
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-27 23:32:42 +0300

    Update Program.cs

--- Exercises/Part 3/Exercise 067/Program.cs
+++ Exercises/Part 3/Exercise 067/Program.cs
@@ -3,21 +3,23 @@ using System.Collections.Generic;
 
 namespace exercise_67
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      List<string> list = new List<string>();
-      while (true)
-      {
-        string input = Console.ReadLine();
-        if (input == "")
+        public static void Main(string[] args)
         {
-          break;
+            List<string> list = new List<string>();
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "")
+                {
+                    break;
+                }
+                list.Add(input);
+            }
+            int count = list.Count;
+            Console.WriteLine(list[0]);
+            Console.WriteLine(list[count - 1]);
         }
-        list.Add(input);
-      }
-
     }
-  }
 }
