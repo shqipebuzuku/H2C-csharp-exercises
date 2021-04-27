8a313c4b4175b52095b8df376cdbb874cb1f84e0
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-28 01:14:34 +0300

    Update Program.cs

--- Exercises/Part 3/Exercise 077/Program.cs
+++ Exercises/Part 3/Exercise 077/Program.cs
@@ -3,14 +3,33 @@ using System.Collections.Generic;
 
 namespace exercise_77
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      // You can try your method here if you want
-    }
+        public static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "")
+                {
+                    break;
+                }
+                list.Add(input);
+            }
+            RemoveLast(list);
+            list.ForEach(Console.WriteLine);
+        }
 
-  }
+        public static void RemoveLast(List<string> list)
+        {
+            int num = list.Count;
+
+            list.RemoveAt(num - 1);
+            
+        }
+
+    }
 }
 
 
