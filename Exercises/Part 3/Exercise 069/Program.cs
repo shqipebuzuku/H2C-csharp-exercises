dc78a007e2fd909caf9b0a20e47843dab5e08eb9
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-27 23:52:05 +0300

    Update Program.cs

--- Exercises/Part 3/Exercise 069/Program.cs
+++ Exercises/Part 3/Exercise 069/Program.cs
@@ -3,21 +3,31 @@ using System.Collections.Generic;
 
 namespace exercise_69
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      List<int> list = new List<int>();
-      while (true)
-      {
-        int input = Convert.ToInt32(Console.ReadLine());
-        if (input == -1)
+        public static void Main(string[] args)
         {
-          break;
+            List<int> list = new List<int>();
+            while (true)
+            {
+                int input = Convert.ToInt32(Console.ReadLine());
+                if (input == -1)
+                {
+                    break;
+                }
+                list.Add(input);
+            }
+            int num = list.Count;
+            
+            Console.WriteLine("From where?");
+            int low = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("To where?");
+            int high = Convert.ToInt32(Console.ReadLine());
+            for (int count = 0;count < num; count++) 
+            { 
+                if (list[count]>=low && list[count] <= high) { Console.WriteLine(list[count]); }
+                
+            }
         }
-        list.Add(input);
-      }
-
     }
-  }
 }
