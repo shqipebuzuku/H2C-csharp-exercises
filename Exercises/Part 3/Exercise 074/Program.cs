c8f65cbdca81ab512b777f7378ae474152559784
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-28 00:40:24 +0300

    Update Program.cs

--- Exercises/Part 3/Exercise 074/Program.cs
+++ Exercises/Part 3/Exercise 074/Program.cs
@@ -3,22 +3,31 @@ using System.Collections.Generic;
 
 namespace exercise_74
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      List<string> list = new List<string>();
-      while (true)
-      {
-        string input = Console.ReadLine();
-        if (input == "")
+        public static void Main(string[] args)
         {
-          break;
+            List<string> list = new List<string>();
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "")
+                {
+                    break;
+                }
+                list.Add(input);
+            }
+            Console.WriteLine("Search for?");
+            string name = Console.ReadLine();
+            int num = list.Count;
+            int flag = 0;
+            for (int count = 0; count < num; count++)
+            {
+                if(name == list[count]) { flag = 1; }
+            }
+            if (flag == 1) { Console.WriteLine(name + " was found!"); }
+            if (flag == 0) { Console.WriteLine(name + " was not found!"); }
         }
-        list.Add(input);
-      }
-      
     }
-  }
 }
 
