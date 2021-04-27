d0e2b66f18930237fdd551856ab1be1a79ea6168
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-28 01:36:02 +0300

    Update Program.cs

--- Exercises/Part 3/Exercise 080/Program.cs
+++ Exercises/Part 3/Exercise 080/Program.cs
@@ -3,22 +3,28 @@ using System.Collections.Generic;
 
 namespace exercise_80
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      int[] numbers = { 5, 1, 3, 4, 2 };
-      int sum = SumOfNumbersInArray(numbers);
-      Console.WriteLine(sum);
+        public static void Main(string[] args)
+        {
+            int[] numbers = { 5, 1, 3, 4, 2 };
+            int sum = SumOfNumbersInArray(numbers);
+            Console.WriteLine(sum);
+
+        }
+
+        public static int SumOfNumbersInArray(int[] numbers)
+        {
+            int sum = 0;
+            int num = numbers.Length;
+            for (int count = 0; count < num; count++)
+            {
+                sum = sum + numbers[count];
+            }
+            return sum;
+        }
 
     }
-
-    public static int SumOfNumbersInArray(int[] numbers) {
-
-      return 0;
-    }
-
-  }
 }
 
 
