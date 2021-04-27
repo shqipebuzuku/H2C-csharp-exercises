d34e91530dbc78f7d15c38edf288d6228e26bd02
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-28 00:21:04 +0300

    Update Program.cs

--- Exercises/Part 3/Exercise 071/Program.cs
+++ Exercises/Part 3/Exercise 071/Program.cs
@@ -3,21 +3,33 @@ using System.Collections.Generic;
 
 namespace exercise_71
 {
-  class Program
-  {
-    public static void Main(string[] args)
+   class Program
     {
-      List<int> list = new List<int>();
-      while (true)
-      {
-        int input = Convert.ToInt32(Console.ReadLine());
-        if (input == -1)
+        public static void Main(string[] args)
         {
-          break;
+            List<int> list = new List<int>();
+            while (true)
+            {
+                int input = Convert.ToInt32(Console.ReadLine());
+                if (input == -1)
+                {
+                    break;
+                }
+                list.Add(input);
+            }
+            int num = list.Count;
+            Console.WriteLine("Search for?");
+            int match = Convert.ToInt32(Console.ReadLine());
+            
+            for (int count = 0;count < num; count++) 
+            { 
+                if (list[count]==match ) 
+                {
+                    Console.WriteLine(match + " is at index " + count);
+                }
+                
+            }
+            
         }
-        list.Add(input);
-      }
-
     }
-  }
 }
