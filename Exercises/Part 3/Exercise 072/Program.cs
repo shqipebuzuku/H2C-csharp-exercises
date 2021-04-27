9198e646a175dddabfddfc3fa44ea4da452e2774
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-28 00:29:28 +0300

    Update Program.cs

--- Exercises/Part 3/Exercise 072/Program.cs
+++ Exercises/Part 3/Exercise 072/Program.cs
@@ -3,11 +3,34 @@ using System.Collections.Generic;
 
 namespace exercise_72
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-
+        public static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            while (true)
+            {
+                int input = Convert.ToInt32(Console.ReadLine());
+                if (input == 9999)
+                {
+                    break;
+                }
+                list.Add(input);
+            }
+            int num = list.Count;
+            
+            int min = 9999;
+            
+            for (int count = 0;count < num; count++) 
+            {
+                if (list[count] <= min) { min = list[count]; }
+                
+            }
+            Console.WriteLine("Smallest number: " + min);
+            for (int count = 0; count < num; count++) 
+            { 
+                if(list[count] == min) { Console.WriteLine("Found at index: " + count); }
+            }
+        }
     }
-  }
 }
