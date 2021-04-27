c067292b232aab04a274b6945e85e03b2bc1edc9
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-28 01:04:51 +0300

    Update Program.cs

--- Exercises/Part 3/Exercise 076/Program.cs
+++ Exercises/Part 3/Exercise 076/Program.cs
@@ -3,14 +3,37 @@ using System.Collections.Generic;
 
 namespace exercise_76
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
+        public static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            while (true)
+            {
+                int input = Convert.ToInt32(Console.ReadLine());
+                if (input == -1)
+                {
+                    break;
+                }
+                list.Add(input);
+            }
+
+            Console.WriteLine(SumNumbers(list));
+            
+        }
+
+        public static int SumNumbers(List<int> number)
+        {
+            int num = number.Count;
+            int sum = 0;
+            for (int count = 0; count < num; count++)
+            {
+                sum = sum + number[count];
+            }
+            return sum;
+        }
 
     }
-
-  }
 }
 
 
