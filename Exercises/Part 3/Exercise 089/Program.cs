73815ba1f292f862fd1b53fdb0bfffd9936dc6d2
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-28 22:24:30 +0300

    Update Program.cs

--- Exercises/Part 3/Exercise 089/Program.cs
+++ Exercises/Part 3/Exercise 089/Program.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 
 namespace exercise_89
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
+        public static void Main(string[] args)
+        {
+            
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "")
+                {
+                    break;
+                }
+                string[] list = input.Split(" ");
+                int num = list.Length;
+                Console.WriteLine(list[num-1]);
+            }
 
-
+        }
     }
-  }
 }
 
 
