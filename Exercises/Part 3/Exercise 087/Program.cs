5cf2a8203b26739ec21cb87e73a0799096532681
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-28 22:19:44 +0300

    Update Program.cs

--- Exercises/Part 3/Exercise 087/Program.cs
+++ Exercises/Part 3/Exercise 087/Program.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 
 namespace exercise_87
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
+        public static void Main(string[] args)
+        {
+            
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "")
+                {
+                    break;
+                }
+                string[] list = input.Split(" ");
+                int num = list.Length;
+                for (int count = 0; count < num; count++) 
+                {
+                    if (list[count].Contains("av")) { Console.WriteLine(list[count]); }
+                }
+            }
 
+        }
     }
-
-  }
 
 }
 
