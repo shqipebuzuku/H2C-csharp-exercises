ae03364c9505ed77678e3b3f93a0d3c0b2202cdf
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-28 20:41:49 +0300

    Update Program.cs

--- Exercises/Part 3/Exercise 084/Program.cs
+++ Exercises/Part 3/Exercise 084/Program.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 
 namespace exercise_84
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
+        public static void Main(string[] args)
+        {
 
+            Console.WriteLine("Give a string");
+            string word = Console.ReadLine();
 
+            if (word == "true") { Console.WriteLine("You got it right!"); }
+            if (word != "true") { Console.WriteLine("Try again!"); }
+        }
     }
-  }
 }
 
