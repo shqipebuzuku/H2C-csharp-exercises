8ece132dcc3494c79712eecd7e4fa002f8b4dfa9
shqipebuzuku <73899103+shqipebuzuku@users.noreply.github.com>
2021-04-30 00:28:38 +0300

    Update Program.cs

--- Exercises/Part 3/Exercise 092/Program.cs
+++ Exercises/Part 3/Exercise 092/Program.cs
@@ -3,13 +3,37 @@ using System.Collections.Generic;
 
 namespace exercise_92
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
+        public static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "")
+                {
+                    break;
+                }
+                list.Add(input);
+            }
 
+            int num = list.Count;
+            string name = "";
+            string longname = "";
+            int maxage = 0;
+            for (int count =0;count<num;count++)
+            {
+                string[] sep = list[count].Split(",");
+                int age = 2020-Convert.ToInt32(sep[1]);
+                name = sep[0];
+                if (name.Length >= longname.Length) { longname = name; }
+                if (age >= maxage) { maxage = age;  }
+            }
+            Console.WriteLine("Longest name: " + longname);
+            Console.WriteLine("Highest age: " + maxage);
+        }
     }
-  }
 }
 
 
